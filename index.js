ba7bdbeeed9219e1dc5a6ee1e8c8ab873968c8be
Okhtay Shoghi <oshoghi@itglue.com>
2017-04-24 14:48:39 -0700

    After benchmarking, changed the string splitting to use a regex.

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-module.exports = function (string, keys) {
+function interpolate (string, keys) {
     if (arguments.length > 2 || typeof(keys) !== "object") {
         keys = Array.prototype.splice.call(arguments, 1);
     }
 
-    var parts = string.split(/(?=[{])|}/);
-    var l = parts.length;
+    var parts = string.split(/(\{[^}]*\})/);
 
-    for (var i = 0; i < l; i += 1) {
+    for (var i = 0; i < parts.length; i++) {
         if (parts[i][0] === "{") {
-            var result = keys[parts[i].slice(1)];
+            var result = keys[parts[i].substring(1, parts[i].length - 1)];
 
             if (typeof(result) !== "undefined") {
                 parts[i] = typeof(result) === "function" ? result() : result;
-            } else {
-                parts[i] = parts[i] + "}";
             }
         }
     }
 
-    return parts.join("");
-};
+    return parts;
+}
+
+function format () {
+    return interpolate.apply(null, arguments).join("");
+}
+
+format.interpolate = interpolate;
+
+module.exports = format;
